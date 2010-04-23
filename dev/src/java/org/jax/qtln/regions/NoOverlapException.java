18ed620091767d50e91c6cf7ab090f07b800462b
dow <devnull@localhost>
2010-04-23 16:37:29 +0000

    Changes to support move of data objects to "regions" package

---
+++ dev/src/java/org/jax/qtln/regions/NoOverlapException.java
@@ -0,0 +1,37 @@
+package org.jax.qtln.regions;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class NoOverlapException extends Throwable
+        implements IsSerializable
+{
+    private static final long serialVersionUID =
+            4741175800356479306L;
+
+    String message;
+
+    public NoOverlapException(Exception e)
+    {
+        message = e.getMessage();
+        
+    }
+
+    public NoOverlapException()
+    {
+    }
+
+    public NoOverlapException(String message)
+    {
+        this.message = message;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
+}
