a09b7dcf9a6a3741509525359b613a8fd6aaef5c
dow <devnull@localhost>
2010-04-23 16:37:16 +0000

    Changes to support move of data objects to "regions" package

---
+++ dev/src/java/org/jax/qtln/regions/InvalidChromosomeException.java
@@ -0,0 +1,38 @@
+package org.jax.qtln.regions;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class InvalidChromosomeException extends Throwable
+        implements IsSerializable
+{
+    private static final long serialVersionUID =
+            4741175800356479306L;
+
+    String message;
+
+    public InvalidChromosomeException(Exception e)
+    {
+        message = e.getMessage();
+        
+    }
+
+    public InvalidChromosomeException()
+    {
+        message = "Not a valid chromosome value";
+    }
+
+    public InvalidChromosomeException(String message)
+    {
+        this.message = message;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
+}
