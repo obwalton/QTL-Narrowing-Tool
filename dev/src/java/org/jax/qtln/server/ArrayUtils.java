d2ad045c1a2a5e9e2d1479d48e61f93999fe8f3f
dow <devnull@localhost>
2010-07-12 13:45:58 +0000

    New module with some basic array utilities

---
+++ dev/src/java/org/jax/qtln/server/ArrayUtils.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jax.qtln.server;
+
+import java.util.List;
+
+/**
+ *
+ * @author dow
+ */
+public class ArrayUtils {
+
+    public static int[] toArray(List<Integer> list) {
+        int[] array = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
+
+    }
+
+    public static int[] toArray(List<Integer> list, int nullDefault) {
+        int[] array = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == null)
+                array[i] = nullDefault;
+            else
+                array[i] = list.get(i);
+        }
+        return array;
+
+    }
+    public static double[] toArray(List<Double> list) {
+        double[] array = new double[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
+
+    }
+
+    public static double[] toArray(List<Double> list, double nullDefault) {
+         double[] array = new double[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == null)
+                array[i] = nullDefault;
+            else
+                array[i] = list.get(i);
+        }
+        return array;
+
+    }
+}
