1e697b71371f850a7fc0752d5735263d66597806
Dave Walton <david.o.walton@gmail.com>
2011-06-11 18:00:33 -0400

    Modifications to support proper terms so it is properly defined as a multi field and Added numeric chromsome for sorting.

--- src/java/org/jax/qtln/mgisearch/MGIQTL.java
+++ src/java/org/jax/qtln/mgisearch/MGIQTL.java
@@ -4,6 +4,7 @@
  */
 package org.jax.qtln.mgisearch;
 
+import java.util.HashMap;
 import org.apache.solr.client.solrj.beans.Field;
 
 /**
@@ -18,16 +19,83 @@ public class MGIQTL {
     @Field("chr")
     String chromosome;
 
+    @Field("chr_num")
+    Integer chromosomeNumber;
+
     @Field("cm")
-    String centimorgans;
+    Float centimorgans;
     
-    @Field("sym")
+    @Field("symbol")
     String symbol;
 
     @Field
     String name;
 
-    @Field("mp")
-    String[][] mpterms;
+    @Field("terms")
+    String[] mpterms;
+
+    public Float getCentimorgans() {
+        return centimorgans;
+    }
+
+    public void setCentimorgans(Float centimorgans) {
+        this.centimorgans = centimorgans;
+    }
+
+    public String getChromosome() {
+        return chromosome;
+    }
+
+    public void setChromosome(String chromosome) {
+        this.chromosome = chromosome;
+        if (chromosome.equals("X")) 
+            this.chromosomeNumber = 20;
+        else if (chromosome.equals("Y"))
+            this.chromosomeNumber = 21;
+        else
+            try {
+            this.chromosomeNumber = new Integer(chromosome);
+            } catch (NumberFormatException nfe) {
+                this.chromosomeNumber = -1;
+            }
+            
+        
+    }
+
+    public String getChromosomeNumber() {
+        return chromosome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String[] getMpterms() {
+        return mpterms;
+    }
+
+    public void setMpterms(String[] mpterms) {
+        this.mpterms = mpterms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
 
   }
