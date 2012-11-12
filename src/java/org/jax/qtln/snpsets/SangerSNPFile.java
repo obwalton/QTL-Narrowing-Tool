4512fee5338270cc7a808bcd65d544aad678806e
Dave Walton <david.o.walton@gmail.com>
2012-11-12 13:58:32 -0500

    Changes to support new UNC SNPS

--- src/java/org/jax/qtln/snpsets/SangerSNPFile.java
+++ src/java/org/jax/qtln/snpsets/SangerSNPFile.java
@@ -22,31 +22,29 @@ import org.jax.qtln.regions.SNPDoesNotMeetCriteriaException;
  *
  * @author dow
  */
-public class SangerSNPFile {
+public class SangerSNPFile extends TabixSearcher {
     
     public static final int STRAIN_START_COLUMN = 9;
     
     private List<String> strains;
-    private TabixReader reader;
     /**
      * 
      * @param vcf
      * 
      */
     public SangerSNPFile(String vcfGz) {
-        
+        super(vcfGz);
         try {
             // Determine what strains are available in this SNP GZ file
             this.strains = getValidSamples(vcfGz);
                         
-            this.reader = new TabixReader(vcfGz);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     
 
-    private List<String> getValidSamples(String vcfGz) 
+    public List<String> getValidSamples(String vcfGz) 
             throws FileNotFoundException, IOException
     {
         System.out.println("In SanterSNPFile.getValidSamples()");
@@ -73,23 +71,6 @@ public class SangerSNPFile {
         return validStrains;       
     }
     
-    public boolean snpInStrains(String s, Map<String, Integer> strains) {
-        String[] tokens = s.split("\t");
-        // TODO: If any of the strains start with 1/1, 1/0 or 0/1 there
-        // is a snp
-        Set<String> keys = strains.keySet();
-        for (String key: keys) {
-            int position = strains.get(key);
-            String value = tokens[position];
-            if (value.startsWith("1/1") || value.startsWith("0/1") || 
-                    value.startsWith("1/0")) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
     /**
      * Do the analysis of a SNP with respect to whether or not it meets our
      * criteria to be kept.
@@ -110,7 +91,7 @@ public class SangerSNPFile {
      *   set of strains, all low responding strains, rs number and source.
      * @throws SNPDoesNotMeetCriteriaException  If for any reason the SNP
      *   fails to pass the criteria this exception is thrown with an
-     *   explaination message.
+     *   explanation message.
      */
     public SNP analyzeSNP(String s, List<String> hrstrains, 
             List<String> lrstrains) 
@@ -204,19 +185,6 @@ public class SangerSNPFile {
         return snp;   
     }
     
-    public TabixReader.Iterator search(String region) {
-        return this.reader.query(region); 
-    }
-    
-    public TabixReader.Iterator search(String chr, int start, int end) {
-        System.out.println("Searching " + chr + ":" + start + "-" + end);
-        //  TODO: Fix bug that causes an out of bounds error if start is 0,
-        //  for now reset to 1
-        if (start == 0)
-            start = 1;
-        return this.search(chr + ":" + start + "-" + end);
-    }
-     
     public List<String> getStrains() {
         return this.strains;
     }
